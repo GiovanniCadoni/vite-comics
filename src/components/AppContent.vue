46575814b30502e9d9ca3fd8daa6cd9b80d54021
GiovanniCadoni <giocadoni19@gmail.com>
2023-11-23 19:07:42 +0100

    add: section of content

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -5,9 +5,24 @@
 </script>
 
 <template>
-    <h2>Content</h2>
+    <div class="container">
+        <section class="content">
+            - Content Goes Here -
+        </section>
+    </div>
 </template>
 
 <style scoped lang="scss">
-
+    .container{
+        background-color: black;
+        .content{
+            width: 80%;
+            margin: auto;
+            color: white;
+            display: flex;
+            align-items: center;
+            /*DEBUG*/
+            min-height: 100px;
+        }
+    }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,34 +4,44 @@
             return {
                 navHeader: [
                     {
-                        title: "CHARACTERS"
+                        title: "CHARACTERS",
+                        isActive: false,
                     },
                     {
-                        title: "COMICS"
+                        title: "COMICS",
+                        isActive: true,
                     },
                     {
-                        title: "MOVIES"
+                        title: "MOVIES",
+                        isActive: false,
                     },
                     {
-                        title: "TV"
+                        title: "TV",
+                        isActive: false,
                     },
                     {
-                        title: "GAMES"
+                        title: "GAMES",
+                        isActive: false,
                     },
                     {
-                        title: "COLLECTIBLES"
+                        title: "COLLECTIBLES",
+                        isActive: false,
                     },
                     {
-                        title: "VIDEOS"
+                        title: "VIDEOS",
+                        isActive: false,
                     },
                     {
-                        title: "FANS"
+                        title: "FANS",
+                        isActive: false,
                     },
                     {
-                        title: "NEWS"
+                        title: "NEWS",
+                        isActive: false,
                     },
                     {
-                        title: "SHOP"
+                        title: "SHOP",
+                        isActive: false,
                     },
                 ],
             };
@@ -40,35 +50,46 @@
 </script>
 
 <template>
-    <div class="header">
-        <img class="logo" src="../assets/img/dc-logo.png" alt="Logo DC">
-        <ul>
-            <li v-for="section in navHeader"><a href="">{{ section.title }}</a></li>
-        </ul>
+    <div class="container">
+        <div class="header">
+            <img class="logo" src="../assets/img/dc-logo.png" alt="Logo DC">
+            <ul>
+                <li v-for="section in navHeader">
+                    <a :class="{ active : section.isActive }" href="">{{ section.title }}</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-    .header{
+    .container{
+        width: 80%;
+        margin: auto;
+        .header{
         height: 100px;
         padding: 80px 0;
         display: flex;
         align-items: center;
         justify-content: space-between;
-        ul{
-            display: flex;
-            gap: 20px;
-            li{
-                list-style: none;
-                a{
-                    text-decoration: none;
-                    color: black;
-                    font-weight: bolder;
-                    &:hover{
-                        color: blue;
+            ul{
+                display: flex;
+                gap: 20px;
+                li{
+                    list-style: none;
+                    a{
+                        text-decoration: none;
+                        color: black;
+                        font-weight: bolder;
+                        &:hover{
+                            color: #0c7cec;
+                        }
+                        &.active{
+                            color: #0c7cec;
+                        }
                     }
                 }
             }
-        }
-    };
+        };
+    }
 </style>
