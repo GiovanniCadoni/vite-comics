edce2b618a2539311a86f0918b5be5a05ad6ca4b
GiovanniCadoni <giocadoni19@gmail.com>
2023-11-23 18:44:18 +0100

    add: header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,74 @@
 <script>
     export default {
-
-    }
+        data() {
+            return {
+                navHeader: [
+                    {
+                        title: "CHARACTERS"
+                    },
+                    {
+                        title: "COMICS"
+                    },
+                    {
+                        title: "MOVIES"
+                    },
+                    {
+                        title: "TV"
+                    },
+                    {
+                        title: "GAMES"
+                    },
+                    {
+                        title: "COLLECTIBLES"
+                    },
+                    {
+                        title: "VIDEOS"
+                    },
+                    {
+                        title: "FANS"
+                    },
+                    {
+                        title: "NEWS"
+                    },
+                    {
+                        title: "SHOP"
+                    },
+                ],
+            };
+        },
+    };
 </script>
 
 <template>
-    <h2>Header</h2>
+    <div class="header">
+        <img class="logo" src="../assets/img/dc-logo.png" alt="Logo DC">
+        <ul>
+            <li v-for="section in navHeader"><a href="">{{ section.title }}</a></li>
+        </ul>
+    </div>
 </template>
 
 <style scoped lang="scss">
-
+    .header{
+        height: 100px;
+        padding: 80px 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        ul{
+            display: flex;
+            gap: 20px;
+            li{
+                list-style: none;
+                a{
+                    text-decoration: none;
+                    color: black;
+                    font-weight: bolder;
+                    &:hover{
+                        color: blue;
+                    }
+                }
+            }
+        }
+    };
 </style>
