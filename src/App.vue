af7e381ad8ac62ce7badc5f5597bed95b2666329
GiovanniCadoni <giocadoni19@gmail.com>
2023-11-23 18:11:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppContent from './components/AppContent.vue'
+import AppNavigation from './components/AppNavigation.vue'
+import AppTopFooter from './components/AppTopFooter.vue'
+import AppBottomFooter from './components/AppBottomFooter.vue'
+
+export default {
+    components: {
+        AppHeader,
+        AppContent,
+        AppNavigation,
+        AppTopFooter,
+        AppBottomFooter,
+    }
+}
+</script>
+
+<template>
+    <AppHeader />
+    <AppContent />
+    <AppNavigation />
+    <AppTopFooter />
+    <AppBottomFooter />
+</template>
+
+<style lang="scss">
+    @use "./style/general.scss";
+</style>
---
+++ src/components/AppBottomFooter.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+
+    }
+</script>
+
+<template>
+    <h2>Bottom-Footer</h2>
+</template>
+
+<style scoped lang="scss">
+
+</style>
